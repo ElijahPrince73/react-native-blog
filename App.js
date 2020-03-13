f3b1978fdeb214afd4b4c1293b436a447ea3fc33
Elijah Prince <elijahprince73@gmail.com>
2020-03-12 22:27:54 -0700

    getting react-navigation setup

--- App.js
+++ App.js
@@ -1,19 +1,20 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react'
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import IndexScreen from './src/screens/indexScreen'
 
-export default function App() {
+const Stack = createStackNavigator();
+
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Blog" component={IndexScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default () => {
+  return <App />
+}
---
+++ src/screens/indexScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text, StyleSheet } from "react-native";
+
+const IndexScreen = () => {
+  return (
+    <View>
+      <Text>INdex screen</Text>
+    </View>
+  )
+}
+
+export default IndexScreen
