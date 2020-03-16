e609ecf4214cdfe31613a5248200c82c211304be
Elijah Prince <elijahprince73@gmail.com>
2020-03-15 20:11:56 -0700

    Can delete blog posts

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -2,8 +2,16 @@ import createDataContext from './createDataContext'
 
 const blogReducer = (state, action) => {
   switch (action.type) {
-    case 'add_blogPost':
-      return [ ...state, { title: `Blog post #${state.length + 1}` }]
+    case 'add_blog_post':
+      return [ 
+          ...state, 
+          { 
+            id: Math.floor(Math.random() * 25), 
+            title: `Blog post #${state.length + 1}` 
+          }
+      ]
+    case 'delete_blog_post':
+      return state.filter((blogPost) => blogPost.id !== action.payload)
     default:
       return state
   }
@@ -11,12 +19,18 @@ const blogReducer = (state, action) => {
 
 const addBlogPost = (dispatch) => {
   return () => {
-    dispatch({ type: 'add_blogPost'  })
+    dispatch({ type: 'add_blog_post'  })
+  }
+}
+
+const deleteBlogPost = (dispatch) => {
+  return (id) => {
+    dispatch({ type: 'delete_blog_post', payload: id})
   }
 }
 
 export const { Context, Provider } = createDataContext(
   blogReducer, 
-  { addBlogPost }, 
+  { addBlogPost, deleteBlogPost }, 
   []
 )
--- src/screens/indexScreen.js
+++ src/screens/indexScreen.js
@@ -1,10 +1,10 @@
 import React, { useContext } from 'react'
-import { View, Text, FlatList, Button, StyleSheet } from "react-native";
+import { View, Text, FlatList, Button, TouchableOpacity, StyleSheet } from "react-native";
 import { Context } from '../context/BlogContext'
 import { Feather } from '@expo/vector-icons'
 
 const IndexScreen = () => {
-  const { state, addBlogPost } = useContext(Context)
+  const { state, addBlogPost, deleteBlogPost } = useContext(Context)
 
   return (
     <View>
@@ -15,7 +15,9 @@ const IndexScreen = () => {
         renderItem={({ item }) => (
           <View style={styles.row}>
             <Text style={styles.title}>{item.title}</Text>
-            <Feather name="trash" style={styles.icon}/>
+            <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
+              <Feather name="trash" style={styles.icon}/>
+            </TouchableOpacity>
           </View>
         )}
       />
